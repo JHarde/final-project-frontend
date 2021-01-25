4c6bee936d6d6758dc7e0afbc1a03001f8eb7b57
Joel Harde <joelharde@Joels-MacBook-Pro.local>
2021-01-25 15:22:14 +0100

    Cleaned up code

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,21 +1,16 @@
 import React from "react";
-import { sortableElement } from "react-sortable-hoc";
 
 export const Vehicles = ({ vehicle }) => {
-  //   const sortableVehicle = sortableElement(({ vehicle }) => (
-  //     <div>{vehicle}</div>
-  //   ));
   return (
-    <div>{vehicle}</div>
-    // <sortableVehicle
-    //   style={{
-    //     height: 50,
-    //     width: 100,
-    //     backgroundColor: "grey",
-    //     margin: 10,
-    //   }}
-    // >
-    //   {vehicle}
-    // </sortableVehicle>
+    <div
+      style={{
+        height: 50,
+        width: 100,
+        backgroundColor: "grey",
+        margin: 10,
+      }}
+    >
+      {vehicle}
+    </div>
   );
 };
