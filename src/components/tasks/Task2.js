4e255c495036abebbc84cc27ca04ceaed6650bea
Joel Harde <joelharde@Joels-MacBook-Pro.local>
2021-01-28 08:51:53 +0100

    Added touch for mobile and tablet devices

--- src/components/tasks/Task2.js
+++ src/components/tasks/Task2.js
@@ -2,22 +2,28 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
+import { useMediaQuery } from "react-responsive";
 
 import { DragAndDropContainer } from "components/DragAndDropContainer";
 import { CompletedTask } from "components/CompletedTask";
 
 export const Task2 = () => {
-  // const handleOnClick = (answer) => {
-  //   answer === question.correctAnswer[0]
-  //     ? setIsCorrect(true)
-  //     : setIsCorrect(false);
-  //   setAnswer(answer);
-  //Send answer to redux and/or backend
-  // };
+  const isDesktopOrLapTop = useMediaQuery({ query: "(min-width: 769px)" });
+  const isMobileOrTablet = useMediaQuery({ query: "(max-width: 768px)" });
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <DragAndDropContainer />
-    </DndProvider>
+    <>
+      {isDesktopOrLapTop && (
+        <DndProvider backend={HTML5Backend}>
+          <DragAndDropContainer />
+        </DndProvider>
+      )}
+      {isMobileOrTablet && (
+        <DndProvider backend={TouchBackend}>
+          <DragAndDropContainer />
+        </DndProvider>
+      )}
+    </>
   );
 };
