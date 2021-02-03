68679444aca5484964cf1fdfeb1e458ddac81685
LillanEriksson <nailsbylillan@hotmail.com>
2021-02-03 13:12:48 +0100

    fixed images for vehicles-task

--- src/components/Vehicles.js
+++ src/components/Vehicles.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React from 'react';
 
 export const Vehicles = ({ vehicle }) => {
-  return (
-    <div
-      style={{
-        height: 50,
-        width: 100,
-        backgroundColor: "grey",
-        margin: 10,
-      }}
-    >
-      {vehicle}
-    </div>
-  );
+	let image = null;
+	switch (vehicle) {
+		case 'Bil':
+			image = 'Car.svg';
+			break;
+		case 'Tåg':
+			image = 'Train.svg';
+			break;
+		case 'Flygplan':
+			image = 'Airplane.svg';
+			break;
+		case 'Gå eller cykla':
+			image = 'Bike.svg';
+			break;
+		case 'Buss':
+			image = 'Bus.svg';
+			break;
+		default:
+			image = null;
+	}
+
+	return <img src={require(`../assets/${image}`)} />;
 };
--- src/components/tasks/Task3.js
+++ src/components/tasks/Task3.js
@@ -1,79 +1,79 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { sortableContainer, sortableElement } from "react-sortable-hoc";
-import arrayMove from "array-move";
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { sortableContainer, sortableElement } from 'react-sortable-hoc';
+import arrayMove from 'array-move';
 
-import { Vehicles } from "components/Vehicles";
-import { CompletedTask } from "components/CompletedTask";
-import { game } from "reducers/game";
-import { TaskContainer } from "components/StyledComponents";
+import { Vehicles } from 'components/Vehicles';
+import { CompletedTask } from 'components/CompletedTask';
+import { game } from 'reducers/game';
+import { TaskContainer } from 'components/StyledComponents';
 
 export const Task3 = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const score = useSelector((store) => store.game.userScore);
+	const score = useSelector((store) => store.game.userScore);
 
-  const question = useSelector((store) => store.game.questions[1]);
+	const question = useSelector((store) => store.game.questions[1]);
 
-  const [answer, setAnswer] = useState();
-  const [isCorrect, setIsCorrect] = useState(false);
+	const [answer, setAnswer] = useState();
+	const [isCorrect, setIsCorrect] = useState(false);
 
-  const answersArray = question.answers;
-  const correctAnswersArray = question.correctAnswer;
+	const answersArray = question.answers;
+	const correctAnswersArray = question.correctAnswer;
 
-  const vehicles = answersArray.map(({ answer }) => answer);
-  const [vehicle, setVehicle] = useState(vehicles);
+	const vehicles = answersArray.map(({ answer }) => answer);
+	const [vehicle, setVehicle] = useState(vehicles);
 
-  const SortableVehiclesContainer = sortableContainer(({ children }) => (
-    <div>{children}</div>
-  ));
+	const SortableVehiclesContainer = sortableContainer(({ children }) => (
+		<div>{children}</div>
+	));
 
-  const SortableVehicle = sortableElement(({ vehicle }) => (
-    <Vehicles key={vehicle} vehicle={vehicle} />
-  ));
+	const SortableVehicle = sortableElement(({ vehicle }) => (
+		<Vehicles key={vehicle} vehicle={vehicle} />
+	));
 
-  const onSortEnd = ({ oldIndex, newIndex }) =>
-    setVehicle(arrayMove(vehicle, oldIndex, newIndex));
+	const onSortEnd = ({ oldIndex, newIndex }) =>
+		setVehicle(arrayMove(vehicle, oldIndex, newIndex));
 
-  const handleOnClick = (answer) => {
-    const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
-    console.log(equals(vehicle, correctAnswersArray));
+	const handleOnClick = (answer) => {
+		const equals = (a, b) => JSON.stringify(a) === JSON.stringify(b);
+		console.log(equals(vehicle, correctAnswersArray));
 
-    if (equals(vehicle, correctAnswersArray)) {
-      setIsCorrect(true);
-      dispatch(game.actions.setUserScore(score + 1));
-    } else {
-      setIsCorrect(false);
-    }
-    setAnswer(vehicle);
-    //Send answer to redux and/or backend
-  };
+		if (equals(vehicle, correctAnswersArray)) {
+			setIsCorrect(true);
+			dispatch(game.actions.setUserScore(score + 1));
+		} else {
+			setIsCorrect(false);
+		}
+		setAnswer(vehicle);
+		//Send answer to redux and/or backend
+	};
 
-  return (
-    <>
-      {!answer && (
-        <TaskContainer>
-          <h1>{question.question}</h1>
-          <div>
-            <SortableVehiclesContainer axis="y" onSortEnd={onSortEnd}>
-              {vehicle.map((item, index) => (
-                <SortableVehicle key={item} vehicle={item} index={index} />
-              ))}
-            </SortableVehiclesContainer>
-            <button type="button" onClick={() => handleOnClick()}>
-              Klar
-            </button>
-          </div>
-        </TaskContainer>
-      )}
-      {answer && (
-        <CompletedTask
-          isCorrect={isCorrect}
-          answer={answer}
-          setAnswer={setAnswer}
-          why={question.why}
-        />
-      )}
-    </>
-  );
+	return (
+		<>
+			{!answer && (
+				<TaskContainer>
+					<h1>{question.question}</h1>
+					<div>
+						<SortableVehiclesContainer axis="y" onSortEnd={onSortEnd}>
+							{vehicle.map((item, index) => (
+								<SortableVehicle key={item} vehicle={item} index={index} />
+							))}
+						</SortableVehiclesContainer>
+						<button type="button" onClick={() => handleOnClick()}>
+							Klar
+						</button>
+					</div>
+				</TaskContainer>
+			)}
+			{answer && (
+				<CompletedTask
+					isCorrect={isCorrect}
+					answer={answer}
+					setAnswer={setAnswer}
+					why={question.why}
+				/>
+			)}
+		</>
+	);
 };
