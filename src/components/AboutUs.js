a6647887bee2703cacb0cae75e8238472a174cfd
LillanEriksson <nailsbylillan@hotmail.com>
2021-02-08 10:37:16 +0100

    added photos to assets

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,22 +1,22 @@
-import React from "react";
+import React from 'react';
 
 export const AboutUs = () => {
-  //link to portfolios
-  //our pitches
-  //what we done in this app
-  //photos of us
-  //link to our LinkedIns
-  return (
-    <div>
-      <div>
-        <h1>Cornelia</h1>
-      </div>
-      <div>
-        <h1>Joel</h1>
-      </div>
-      <div>
-        <h1>Lillemor</h1>
-      </div>
-    </div>
-  );
+	//link to portfolios
+	//our pitches
+	//what we done in this app
+	//photos of us
+	//link to our LinkedIns
+	return (
+		<div>
+			<div>
+				<h1>Cornelia</h1>
+			</div>
+			<div>
+				<h1>Joel</h1>
+			</div>
+			<div>
+				<h1>Lillemor</h1>
+			</div>
+		</div>
+	);
 };
