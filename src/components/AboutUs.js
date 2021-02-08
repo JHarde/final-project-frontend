746065a4449bda2061e87a74745d314e916d29e1
LillanEriksson <nailsbylillan@hotmail.com>
2021-02-08 11:37:47 +0100

    Starting with the about us page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import {
+	ProfileImage,
+	ProfileImageContainer,
+	AboutUsContainer,
+	PersonContainer,
+} from './StyledComponents';
+
 export const AboutUs = () => {
 	//link to portfolios
 	//our pitches
@@ -7,16 +14,67 @@ export const AboutUs = () => {
 	//photos of us
 	//link to our LinkedIns
 	return (
-		<div>
+		<AboutUsContainer>
+			<PersonContainer>
+				{' '}
+				<ProfileImageContainer>
+					<ProfileImage
+						src={require('../assets/Cornelia.png')}
+						alt="Cornelia"
+					/>
+				</ProfileImageContainer>
+				<h1>Cornelia Ryås</h1>
+				<p>Frontend-developer/motion-designer</p>
+				<div>
+					{' '}
+					<img
+						src="https://img.icons8.com/color/48/000000/linkedin.png"
+						alt="LinkedIn"
+					/>
+					<img src="https://img.icons8.com/fluent/48/000000/github.png" />
+				</div>
+			</PersonContainer>
+			<PersonContainer>
+				{' '}
+				<ProfileImageContainer>
+					<ProfileImage src={require('../assets/Joel.png')} alt="Joel" />
+				</ProfileImageContainer>
+				<h1>Joel Harde</h1> <p>Frontend-developer/lärare</p>
+				<div>
+					{' '}
+					<img
+						src="https://img.icons8.com/color/48/000000/linkedin.png"
+						alt="LinkedIn"
+					/>
+					<img src="https://img.icons8.com/fluent/48/000000/github.png" />
+				</div>
+			</PersonContainer>
+			<PersonContainer>
+				<ProfileImageContainer>
+					<ProfileImage
+						src={require('../assets/Lillemor.png')}
+						alt="Lillemor"
+					/>
+				</ProfileImageContainer>
+				<h1>Lillemor Eriksson</h1>
+				<p>Frontend-developer med bakgrund som entreprenör</p>
+				<div>
+					{' '}
+					<img
+						src="https://img.icons8.com/color/48/000000/linkedin.png"
+						alt="LinkedIn"
+					/>
+					<img src="https://img.icons8.com/fluent/48/000000/github.png" />
+				</div>
+			</PersonContainer>
 			<div>
-				<h1>Cornelia</h1>
+				<a href="https://icons8.com/icon/13930/linkedin">
+					LinkedIn icon by Icons8
+				</a>
+				<a href="https://icons8.com/icon/AZOZNnY73haj/github">
+					GitHub icon by Icons8
+				</a>
 			</div>
-			<div>
-				<h1>Joel</h1>
-			</div>
-			<div>
-				<h1>Lillemor</h1>
-			</div>
-		</div>
+		</AboutUsContainer>
 	);
 };
--- src/components/StyledComponents.js
+++ src/components/StyledComponents.js
@@ -25,6 +25,35 @@ export const Container = styled.section`
 	}
 `;
 
+export const AboutUsContainer = styled.section`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+
+	min-height: 100vh;
+	background-color: ${beige};
+	padding: 50px;
+`;
+
+export const PersonContainer = styled.section`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	padding: 10px;
+`;
+
+export const ProfileImage = styled.img`
+	max-width: 100%;
+	height: auto;
+	object-fit: contain;
+	border-radius: 50%;
+`;
+
+export const ProfileImageContainer = styled.div`
+	width: 200px;
+	height: 200px;
+`;
+
 export const DesktopLogin = styled.div`
 	display: flex;
 	flex-direction: column;
@@ -43,7 +72,6 @@ export const TaskContainer = styled(Container)`
 		flex-direction: column;
 		justify-content: center;
 	}
-
 `;
 
 export const EndGameContainer = styled(Container)`
@@ -235,8 +263,15 @@ export const Button = styled.button`
 
 export const GameCategory = styled(Button)`
 	height: 155px;
-	background-color: ${(props) => (props.color === 'red' ? red : props.color === 'beige' ? beige : lightGreen)};
-	box-shadow: 3px 5px ${(props) => (props.color === 'red' ? brown : props.color === 'beige' ? '#CDC5B5' : green)};
+	background-color: ${(props) =>
+		props.color === 'red' ? red : props.color === 'beige' ? beige : lightGreen};
+	box-shadow: 3px 5px
+		${(props) =>
+			props.color === 'red'
+				? brown
+				: props.color === 'beige'
+				? '#CDC5B5'
+				: green};
 `;
 
 export const TaskButton = styled(Button)`
