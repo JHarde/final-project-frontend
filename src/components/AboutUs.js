ff94861d71b297a933fd911803cc75ead8220342
LillanEriksson <nailsbylillan@hotmail.com>
2021-02-10 11:55:29 +0100

    added info about lillemor and joel

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -54,11 +54,22 @@ export const AboutUs = () => {
 					</a>
 				</ContactIconsContainer>
 				<ProfileText>
-					Cheese on toast airedale the big cheese. Danish fontina cheesy grin
-					airedale danish fontina taleggio the big cheese macaroni cheese
-					port-salut. Edam fromage lancashire feta caerphilly everyone loves
-					chalk and cheese brie. Red leicester parmesan cheese and biscuits
-					cheesy feet blue castello cheesecake fromage frais smelly cheese.
+					<p>
+						{' '}
+						Cheese on toast airedale the big cheese. Danish fontina cheesy grin
+						airedale danish fontina taleggio the big cheese macaroni cheese
+						port-salut. Edam fromage lancashire feta caerphilly everyone loves
+						chalk and cheese brie. Red leicester parmesan cheese and biscuits
+						cheesy feet blue castello cheesecake fromage frais smelly cheese.
+					</p>
+					<p>
+						{' '}
+						Cheese on toast airedale the big cheese. Danish fontina cheesy grin
+						airedale danish fontina taleggio the big cheese macaroni cheese
+						port-salut. Edam fromage lancashire feta caerphilly everyone loves
+						chalk and cheese brie. Red leicester parmesan cheese and biscuits
+						cheesy feet blue castello cheesecake fromage frais smelly cheese.
+					</p>
 				</ProfileText>
 			</PersonContainer>
 			<PersonContainer>
@@ -96,11 +107,22 @@ export const AboutUs = () => {
 					</a>
 				</ContactIconsContainer>
 				<ProfileText>
-					Cheese on toast airedale the big cheese. Danish fontina cheesy grin
-					airedale danish fontina taleggio the big cheese macaroni cheese
-					port-salut. Edam fromage lancashire feta caerphilly everyone loves
-					chalk and cheese brie. Red leicester parmesan cheese and biscuits
-					cheesy feet blue castello cheesecake fromage frais smelly cheese.
+					<p>
+						Min resa från förskolepedagog till front end utvecklare började för
+						snart ett halvår sedan. I programmering såg jag en möjlighet att
+						utforska min kreativa sida och fick chansen att börja bygga appar
+						och hemsidor från grunden. Jag tycker om kombinationen av att arbeta
+						kreativt och samtidigt få använda mig av mina kunskaper kring
+						problemlösning för att hitta nya lösningar.
+					</p>
+					<p>
+						My journey from working as a kindergarten teacher to becoming a
+						front end developer started about six months ago. In programming I
+						saw an opportunity to develop my creative side and to create
+						applications and websites from scratch. The combination of working
+						in a creative way and finding solutions on various problems brings
+						me a lot of joy.
+					</p>
 				</ProfileText>
 			</PersonContainer>
 			<PersonContainer>
@@ -142,11 +164,25 @@ export const AboutUs = () => {
 					</a>
 				</ContactIconsContainer>
 				<ProfileText>
-					Cheese on toast airedale the big cheese. Danish fontina cheesy grin
-					airedale danish fontina taleggio the big cheese macaroni cheese
-					port-salut. Edam fromage lancashire feta caerphilly everyone loves
-					chalk and cheese brie. Red leicester parmesan cheese and biscuits
-					cheesy feet blue castello cheesecake fromage frais smelly cheese.
+					<p>
+						Jag har en bakgrund som kreativ entreprenör. Som entreprenör har jag
+						lärt mig att lösa problem på olika sätt, och mitt mindset är att
+						alla problem har en lösning. Jag älskar att skapa användbara saker
+						som löser problem för andra människor. Jag brinner för att lära mig
+						nya saker, och just därför älskar jag att koda, för det finns alltid
+						mer att lära och nytt att upptäcka. Mitt mål är i första hand att
+						jobba som frontend-developer, men jag har planer att i framtiden bli
+						fullstack.
+					</p>
+					<p>
+						I have a background as a creative entrepreneur. As an entrepreneur
+						I've learned to tackle obstacles in different ways, and my mindset
+						is that every problem has a solution. I love to create useful things
+						that solves problems for other people. I'm passionate about learning
+						new things, and I love coding, because there's always more to learn!
+						My goal is to first be a frontend-developer, but in the future I
+						have ambitions to go fullstack.
+					</p>
 				</ProfileText>
 			</PersonContainer>
 			<div>
