03804492e7ee9c64760273848330459cd139d2d7
Joel Harde <joelharde@Joels-MacBook-Pro.local>
2021-02-12 09:05:01 +0100

    Added text in readme on accessibility and dnd

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 A game built in React aimed towards children to create awarness of and interest in the climate change. 
 
 1. Create an account/login or choose to play as guest. 
-2. Choose one of three games to play
+2. Choose one of three games to play.
 3. The games are either to answer a question or to drag and drop objects.
-4. Choose to finish the game or continue by playing one of the other games
-5. When statified choose to end the game and send your result to the highscore list
+4. Choose to finish the game or continue by playing one of the other games.
+5. When statified choose to end the game and send your result to the highscore list.
 
 ## Process
 We set the basic structure and sketch of MVP in Figma before starting to code. This helped us to know where to start and made it easier to divide the tasks.
@@ -15,7 +15,11 @@ We worked in branches and merged when needed without any major issues. This also
 Our MVP was to have three questions a user could play and then send their score to the highscore, without Login. 
 We fetched the questions from our backend and used useState and conditional rendering to handle the response to show different pages depending on the answer.
 We use dispatch to update the score in redux if the answer was correct.
-After testing in CodePen we used two different libraries for the drag and drop functions. ......
+After testing in CodePen we used two different libraries for the drag and drop functions. One library is called React-sortable-hoc and
+gives the user the possibility to sort items in different orders. It's found in task 3 where the user can
+sort vehicles in the order the user sees fit. The other libraries is called React-Dnd-html5-backend and React-dnd-touch-backend and
+makes it possible for the user to drag an item and drop it into a box or a container. In our application we've used it in task 2
+where the user can recycle different items into specific trash bins. If the user places all items correctly he or she will recieve one point. The same goes for task 3 where the user gets a point when sorting the vehicles in the correct order. 
 The user can choose to end the game and then will be shown a oppurtunity to send their score to the highscore list together with a chosen user name. 
 
 When this was in place we made a more final design of the UI in Figma and styled accordingly with styled components and imported cutom made illustrations. 
@@ -42,12 +46,22 @@ We have POST endpoints for login, signup and logout. Signup (/users) creates a u
 - Mongoose
 
 ### Libraries
-- React dnd-html-backend (Drag and Drop)
+- React dnd-html5-backend (Drag and Drop)
 - React dnd-touch-backend (Drag and Drop)
 - React Sortable Hoc (Drag and drop)
 - React Responsive
 
 ## Accessibility
+We have worked with our application in a way to secure it will be accessible for all people. 
+
+We have tested our application in both Lighthouse and Wave to check the accessibility. The application scores
+as following on Lighthouse:
+
+- Accessibility: 90
+- Best practices: 100
+- SEO: 92
+
+In Wave our application passes the test with no errors or contrast errors. 
 
 ## View it live
 https://klimatspelet.netlify.app/
--- src/components/EndGame.js
+++ src/components/EndGame.js
@@ -1,91 +1,92 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import {
-	Header,
-	EndGameContainer,
-	Paragraph,
-	HighscoreLabel,
-	HeaderScore,
-	Button,
-	Input,
-	HeaderScoreContainer,
-	HeaderScorePoints,
-	ScoreParagraph
-} from 'components/StyledComponents';
-import { fetchHighscore, postHighscore } from 'reducers/game';
-import { user } from 'reducers/user';
-import { game } from 'reducers/user';
-import { Highscore } from 'components/Highscore'
-import { Logout } from 'components/Logout'
+  Header,
+  EndGameContainer,
+  Paragraph,
+  HighscoreLabel,
+  HeaderScore,
+  Button,
+  Input,
+  HeaderScoreContainer,
+  HeaderScorePoints,
+  ScoreParagraph,
+} from "components/StyledComponents";
+import { fetchHighscore, postHighscore } from "reducers/game";
+import { user } from "reducers/user";
+import { game } from "reducers/user";
+import { Highscore } from "components/Highscore";
+import { Logout } from "components/Logout";
 
 export const EndGame = () => {
-	const dispatch = useDispatch();
-	const guestScore = useSelector((store) => store.game.guestScore);
-	const accessToken = useSelector((store) => store.user.accessToken);
-	const score = useSelector((store) => store.user.score)
-	const userId = useSelector((store) => store.user.userId)
-	const userName = useSelector((store) => store.user.userName)
+  const dispatch = useDispatch();
+  const guestScore = useSelector((store) => store.game.guestScore);
+  const accessToken = useSelector((store) => store.user.accessToken);
+  const score = useSelector((store) => store.user.score);
+  const userId = useSelector((store) => store.user.userId);
+  const userName = useSelector((store) => store.user.userName);
 
+  const [avatarName, setAvatarName] = useState("");
+  const [isSent, setIsSent] = useState(false);
 
-	const [avatarName, setAvatarName] = useState('');
-	const [isSent, setIsSent] = useState(false);
+  const sendScore = (event) => {
+    //Get the logged in user's name to POST instead of avatarName
 
-	const sendScore = (event) => {
-		//Get the logged in user's name to POST instead of avatarName
+    fetch("https://environmental-kids-game.herokuapp.com/highscore", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: accessToken ? userName : avatarName,
+        score: accessToken ? score : guestScore,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setIsSent(true);
+      });
+    event.preventDefault();
+  };
+  if (!isSent) {
+    return (
+      <EndGameContainer>
+        <HeaderScoreContainer>
+          <HeaderScore>{accessToken ? score : guestScore}</HeaderScore>
+          <ScoreParagraph>poäng</ScoreParagraph>
+        </HeaderScoreContainer>
 
-		fetch('https://environmental-kids-game.herokuapp.com/highscore', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ name: accessToken ? userName : avatarName, score: accessToken ? score : guestScore }),
-		})
-			.then((res) => res.json())
-			.then((res) => {
-				console.log(res);
-				setIsSent(true);
-			});
-		event.preventDefault();
-
-	};
-	if(!isSent){
-	return (
-		<EndGameContainer>
-			<HeaderScoreContainer>
-				<HeaderScore>{accessToken ? score : guestScore}</HeaderScore>
-				<ScoreParagraph>poäng</ScoreParagraph>
-			</HeaderScoreContainer>
-
-			<Header>Vill du vara med på topplistan?</Header>
-			<form>{!accessToken &&
-				<HighscoreLabel>
-					Vad heter din Avatar?
-					<Input
-						type="text"
-						value={avatarName}
-						onChange={(event) => setAvatarName(event.target.value)}
-						placeholder="Skriv ett namn här"
-						maxLength="20"
-					/>
-				</HighscoreLabel>}
-				<Button type="submit" onClick={sendScore} style={{fontSize: 27}}>
-					Skicka till topplista
-				</Button>
-			</form>
-			{accessToken &&
-			<Logout button/>}
-			{/* <Link to="/">
+        <Header>Vill du vara med på topplistan?</Header>
+        <form>
+          {!accessToken && (
+            <HighscoreLabel>
+              Vad heter din Avatar?
+              <Input
+                type="text"
+                value={avatarName}
+                onChange={(event) => setAvatarName(event.target.value)}
+                placeholder="Skriv ett namn här"
+                maxLength="20"
+              />
+            </HighscoreLabel>
+          )}
+          <Button type="submit" onClick={sendScore} style={{ fontSize: 27 }}>
+            Skicka till topplista
+          </Button>
+        </form>
+        {accessToken && <Logout button />}
+        {/* <Link to="/">
 				{' '}
 				 Nollställa state/redux?
 				<Button>Spela igen</Button>
 			</Link> */}
-			{/* <Link to="/highscore">
+        {/* <Link to="/highscore">
 				<Button>Se topplista</Button>
 			</Link> */}
-		</EndGameContainer>
-	);
-	} else {
-		return <Highscore/>
-	}
-
+      </EndGameContainer>
+    );
+  } else {
+    return <Highscore />;
+  }
 };
