9f0d20c9d816329fa9323457ea2eccc27569ada8
LillanEriksson <nailsbylillan@hotmail.com>
2021-02-12 13:53:35 +0100

    please work

--- src/components/tasks/Task2.js
+++ src/components/tasks/Task2.js
@@ -3,32 +3,12 @@ import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
 import { useMediaQuery } from 'react-responsive';
-import MultiBackend, {
-	createTransition,
-	Preview,
-} from 'react-dnd-multi-backend';
 
 import { DragAndDropContainer } from 'components/DragAndDropContainer';
 
 export const Task2 = () => {
 	const isDesktopOrLapTop = useMediaQuery({ query: '(min-width: 769px)' });
 	const isMobileOrTablet = useMediaQuery({ query: '(max-width: 768px)' });
-
-	// const TouchTransition = createTransition('touchstart', (event) => {
-	// 	return event.touches != null;
-	// });
-
-	// const Touch = {
-	// 	backends: [
-	// 		{ backend: HTML5Backend },
-	// 		{
-	// 			backend: TouchBackend,
-	// 			options: { enableMouseEvents: true },
-	// 			preview: true,
-	// 			transition: TouchTransition,
-	// 		},
-	// 	],
-	// };
 
 	return (
 		<>
