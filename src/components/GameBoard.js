7922b5b027fbb8fb3cbab6689242549943b57cfa
Joel Harde <joelharde@Joels-MacBook-Pro.local>
2021-02-12 09:30:53 +0100

    Removed logout button from gameboard

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,92 +1,92 @@
-import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { useMediaQuery } from 'react-responsive';
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useMediaQuery } from "react-responsive";
 
-import { Logout } from 'components/Logout';
-import { HamburgerMenu } from './HamburgerMenu';
+import { Logout } from "components/Logout";
+import { HamburgerMenu } from "./HamburgerMenu";
 
-import { fetchQuestions } from 'reducers/game';
+import { fetchQuestions } from "reducers/game";
 
 import {
-	Container,
-	GameCategory,
-	Title,
-	CategoryImage,
-	CategoryText,
-} from 'components/StyledComponents';
+  Container,
+  GameCategory,
+  Title,
+  CategoryImage,
+  CategoryText,
+} from "components/StyledComponents";
 
-import WaterDrop from '../assets/WaterDrop.svg';
-import Bike from '../assets/Bike_Silhouette.svg';
-import Recycle from '../assets/Recycle.svg';
+import WaterDrop from "../assets/WaterDrop.svg";
+import Bike from "../assets/Bike_Silhouette.svg";
+import Recycle from "../assets/Recycle.svg";
 
 export const GameBoard = () => {
-	const dispatch = useDispatch();
-	const accessToken = useSelector((store) => store.user.accessToken);
+  const dispatch = useDispatch();
+  const accessToken = useSelector((store) => store.user.accessToken);
 
-	const isDesktopOrLapTop = useMediaQuery({ query: '(min-width: 769px)' });
-	const isMobileOrTablet = useMediaQuery({ query: '(max-width: 768px)' });
-	useEffect(() => {
-		dispatch(fetchQuestions());
-	}, [dispatch]);
+  const isDesktopOrLapTop = useMediaQuery({ query: "(min-width: 769px)" });
+  const isMobileOrTablet = useMediaQuery({ query: "(max-width: 768px)" });
+  useEffect(() => {
+    dispatch(fetchQuestions());
+  }, [dispatch]);
 
-	return (
-		<>
-			{isDesktopOrLapTop && (
-				<Container>
-					<HamburgerMenu />
-					{accessToken && <Logout />}
-					<div>
-						<Title>Välj Spel</Title>
-						<div>
-							<Link to="/task1">
-								<GameCategory color="beige">
-									<CategoryImage src={WaterDrop} alt="Water" />
-									<CategoryText>Vatten</CategoryText>
-								</GameCategory>
-							</Link>
-							<Link to="/task2">
-								<GameCategory color="red">
-									<CategoryImage src={Recycle} alt="Recycle" />
-									<CategoryText>Återvinning</CategoryText>
-								</GameCategory>
-							</Link>
-							<Link to="/task3">
-								<GameCategory>
-									<CategoryImage src={Bike} alt="Bike" />
-									<CategoryText>Resande</CategoryText>
-								</GameCategory>
-							</Link>
-						</div>
-					</div>
-				</Container>
-			)}
+  return (
+    <>
+      {isDesktopOrLapTop && (
+        <Container>
+          <HamburgerMenu />
 
-			{isMobileOrTablet && (
-				<Container>
-					<HamburgerMenu />
-					{accessToken && <Logout />}
-					<Title>Välj Spel</Title>
-					<Link to="/task1">
-						<GameCategory color="beige">
-							<CategoryImage src={WaterDrop} alt="Water" />
-							<CategoryText>Vatten</CategoryText>
-						</GameCategory>
-					</Link>
-					<Link to="/task2">
-						<GameCategory color="red">
-							<CategoryImage src={Recycle} alt="Recycle" />
-							<CategoryText>Återvinning</CategoryText>
-						</GameCategory>
-					</Link>
-					<Link to="/task3">
-						<GameCategory>
-							<CategoryImage src={Bike} alt="Bike" />
-							<CategoryText>Resande</CategoryText>
-						</GameCategory>
-					</Link>
-				</Container>
-			)}
-		</>
-	);
+          <div>
+            <Title>Välj Spel</Title>
+            <div>
+              <Link to="/task1">
+                <GameCategory color="beige">
+                  <CategoryImage src={WaterDrop} alt="Water" />
+                  <CategoryText>Vatten</CategoryText>
+                </GameCategory>
+              </Link>
+              <Link to="/task2">
+                <GameCategory color="red">
+                  <CategoryImage src={Recycle} alt="Recycle" />
+                  <CategoryText>Återvinning</CategoryText>
+                </GameCategory>
+              </Link>
+              <Link to="/task3">
+                <GameCategory>
+                  <CategoryImage src={Bike} alt="Bike" />
+                  <CategoryText>Resande</CategoryText>
+                </GameCategory>
+              </Link>
+            </div>
+          </div>
+        </Container>
+      )}
+
+      {isMobileOrTablet && (
+        <Container>
+          <HamburgerMenu />
+
+          <Title>Välj Spel</Title>
+          <Link to="/task1">
+            <GameCategory color="beige">
+              <CategoryImage src={WaterDrop} alt="Water" />
+              <CategoryText>Vatten</CategoryText>
+            </GameCategory>
+          </Link>
+          <Link to="/task2">
+            <GameCategory color="red">
+              <CategoryImage src={Recycle} alt="Recycle" />
+              <CategoryText>Återvinning</CategoryText>
+            </GameCategory>
+          </Link>
+          <Link to="/task3">
+            <GameCategory>
+              <CategoryImage src={Bike} alt="Bike" />
+              <CategoryText>Resande</CategoryText>
+            </GameCategory>
+          </Link>
+        </Container>
+      )}
+    </>
+  );
 };
